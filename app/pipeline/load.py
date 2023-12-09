93c75224f6aceb4af27e5d369ba7a6bd64a92b1a
vorges-data <vinicius.jborges36@gmail.com>
2023-12-09 16:24:05 -0300

    Transform e Load concluídos e funcionando!

---
+++ app/pipeline/load.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+
+
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+
+    """
+        ecebe um dataframe e salva em arquivo excel
+
+        args: 
+            data_frame (pd.DataFrame): dataframe a ser salvo como excel
+            output_path (str): caminho do arquivo de saída
+            file_name (str): nome do arquivo de saída
+
+        return: "Arquivo salvo com sucesso!"
+
+    """
+
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+
+    # salva o arquivo excel
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
+
+    # retorna a mensagem de sucesso
+    return "Arquivo salvo com sucesso!"
